18c5e0317b5b393de574317d62c778c625f08e50
Gordon Fontenot <gordon.fontenot@me.com>
2012-04-07 10:42:49 -0400

    Clean gh-projects a bit
    Improve some of the variable names, fix some of the comments. No big.

--- js/gh-projects.js
+++ js/gh-projects.js
@@ -1,26 +1,31 @@
+$(document).ready( function() {
+  get_github_public_repos('gfontenot');
+});
+
 function get_github_public_repos(username) {  
   // Github API V3 endpoint
-  var gh_pub_repos = "https://api.github.com/users/" + username + "/repos";
+  var gh_pub_repos_url = "https://api.github.com/users/" + username + "/repos";
 
   $.ajax({
-    url: gh_pub_repos,
+    url: gh_pub_repos_url,
     dataType: 'jsonp',
     type: 'get',
-    success: function(data) {
+    success: function(response) {
       // The data from the response is the array of repos
-      var repos = data.data;
+      var repos = response.data;
 
       // Function to sort by date inversely
       function sort_by_date(a, b) {
         return new Date(b.pushed_at) - new Date(a.pushed_at);
       }
+
       repos.sort(sort_by_date);
 
       var i;
       for(i = 0; i < repos.length; i++){
         var repo = repos[i];
 
-        // Only show the forks, but we do want to link to dotfiles
+        // Don't show forked repos, unless it's the dotfiles repo
         if (repo.name == 'dotfiles' || !repo.fork) {
 
           // Create an <a> for the repo name linked to the github url
